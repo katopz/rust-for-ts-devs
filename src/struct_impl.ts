bf5bc68690a5393fe99cb92e3f2e9a9aec7e5dbe
katopz <katopz@gmail.com>
2022-05-07 19:08:05 +0700

    use function

--- src/struct_impl.ts
+++ src/struct_impl.ts
@@ -1,4 +1,4 @@
-import { create_struct_with_foo, MyStruct } from "./structs";
+import { MyStruct } from "./structs";
 
 // Adding methods to a type is done using an `impl` block:
 
@@ -6,17 +6,13 @@ class MyNewStruct extends MyStruct {
     /// "Constructor" (static method):
     constructor() {
         super("hi", 0);
-
-        // Or use functional style:
-        const anotherMyNewStruct = create_struct_with_foo("hello");
-        console.log(`🦞 anotherMyNewStruct: ${anotherMyNewStruct.foo}`);
     }
 
     /// Regular methods take a reference to `self`:
     public print_foo() {
-        console.log(`Foo: ${this.foo}`);
+        console.log(`🦞 Foo: ${this.foo}`);
     }
 }
 
 /// # Output
-console.log(`🦞 ${new MyNewStruct().foo}`);
+new MyNewStruct().print_foo();
